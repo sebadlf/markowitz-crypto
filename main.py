e3b341b893ddb4ffb53e5c9470bc6385444bc286
luisaoisdead <luis_idc@hotmail.com>
2020-10-15 19:29:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ import utils
 
 
 # Obtiene el top 50 de tickers ordenados por volumen
-top = cryptocompare.get_top_tickers(50, cache_days=30)
+#top = cryptocompare.get_top_tickers(50, cache_days=30)
+top = cryptocompare.get_top_tickers(cantidad =7,cache_days=30)
 
 tickers = top.ticker
 
 # Obtiene el historico de precios de los tickers pasados como parametros
 prices = cryptocompare.get_mutiple_close_prices(tickers, cache_days= 1)
 
-evolution = markowitz_evolution(prices, step=7, count=52, q_inicial=1500)
+evolution = markowitz_evolution(prices, step=7, count=5, q_inicial=1500)
 utils.save('mark', evolution) 
