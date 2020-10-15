190c3019ccbb6e7cc26d69b595c2b4aefeadc706
Sebastián <sebadlf@gmail.com>
2020-10-15 19:23:40 -0300

    Agrego sma y rsi

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 import random
 import datetime as dt
 import pandas as pd
+import numpy as np
 
 def is_older_than(filename, days):
     time = os.path.getmtime('./files/' + filename + '.h5') 
@@ -35,3 +36,69 @@ def sample_sin_repetir(list, k):
             result.append(item)
     
     return result
+
+def get_operaciones(data):
+    df = data.copy()
+
+    # Una sola entrada y salida por vez
+    trades = df.loc[df.signal != 'hold'].copy()
+    trades['signal'] = np.where(trades.signal != trades.signal.shift(), trades.signal, 'hold')
+    trades = trades.loc[trades.signal != 'hold'].copy()
+    
+    # Supuesto estrategia long, debe empezar con compra y terminar con venta
+    if trades.iloc[0]['signal'] =='sell':
+        trades = trades.iloc[1:]
+
+    if trades.iloc[-1]['signal']=='buy':
+        trades = trades.iloc[:-1]
+
+    fechas_compra = trades.iloc[::2].index
+    fechas_venta = trades.iloc[1::2].index
+
+    operaciones = (fechas_compra).to_frame()
+    operaciones['fecha_venta'] = fechas_venta
+    
+    operaciones.columns = ['fecha_compra', 'fecha_venta']
+    
+    return operaciones
+
+def agrego_indicadores(prices):
+    retornos = np.log((prices/prices.shift(1)))
+
+    for ticker in prices.columns:
+        sma_5 = prices[ticker].rolling(5).mean()
+        sma_10 = prices[ticker].rolling(10).mean()
+    
+        comprado = (sma_5 / sma_10) >= 1
+        
+        retornos['estoyComprado'] = comprado
+        
+        retornos[ticker+'_sma'] = np.where(retornos.estoyComprado, retornos[ticker], 0)    
+        
+        rsi_q = 9
+        
+        dif = prices[ticker].diff()
+        win =  pd.DataFrame(np.where(dif > 0, dif, 0))
+        loss = pd.DataFrame(np.where(dif < 0, abs(dif), 0))
+        ema_win = win.ewm(alpha=1/rsi_q).mean()
+        ema_loss = loss.ewm(alpha=1/rsi_q).mean()
+        rs = ema_win / ema_loss
+        rsi = 100 - (100 / (1+rs))
+        rsi.index = retornos.index
+        retornos['rsi'] = rsi   
+        
+        retornos['signal'] = np.where(retornos.rsi < 30, 'buy', np.where(retornos.rsi > 70, 'sell', 'hold'))
+        
+        operaciones = get_operaciones(retornos)
+        
+        retornos['estoyComprado'] = False
+        
+        for i in range(len(operaciones)):
+            op = operaciones.iloc[i]
+            retornos['estoyComprado'] = np.where((retornos.index >= op.fecha_compra) & (retornos.index <= op.fecha_venta), True, False)
+        
+        retornos[ticker+'_rsi'] = np.where(retornos.estoyComprado, retornos[ticker], 0) 
+    
+    retornos = retornos.drop(['estoyComprado', 'rsi', 'signal'], axis=1)    
+    
+    return retornos 
